6c7e77ba809de220886a2f3ad0e7e595d25dae54
Alejandro <alejandro.carayol@gmail.com>
2023-12-07 18:37:59 +0100

    Terminado

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
@@ -7,6 +7,48 @@ import { Component } from '@angular/core';
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
+  
+  @ViewChild("formName")
+  formName!: ElementRef;
 
+  @ViewChild("formCIF")
+  formCIF!: ElementRef;
+
+  @ViewChild("formDirection")
+  formDirection!: ElementRef;
+
+  @ViewChild("formGroup")
+  formGroup!: ElementRef;
+
+  @Output() onGuardar: EventEmitter<string[]> = new EventEmitter<string[]>();
+
+  constructor() {
+    
+  }
+ 
+  ngOnInit() {
+  }
+
+  clickGuardar = (): void => {
+    let formData: string[] = [];
+    console.log(this.formName.nativeElement.value);
+    formData.push(this.formName.nativeElement.value);
+    formData.push(this.formCIF.nativeElement.value);
+    formData.push(this.formDirection.nativeElement.value);
+    formData.push(this.formGroup.nativeElement.value);
+
+    let valid: boolean = true;
+    for (let i = 0; i < formData.length; i++) {
+      if(formData[i].trim() == ""){
+        valid = false;
+      }
+      
+    }
+
+    console.log(formData);
+    if (valid) {
+      this.onGuardar.emit(formData);
+    }
+  }
 }
